782e642d0c8408ba900c1515d9e4a3716a5e6197
TrucDev <jackyhuynh87@gmail.com>
2021-12-02 13:00:28 -0500

    Update main.py

---
+++ Basic 32 Birthday Wisher/main.py
@@ -0,0 +1,24 @@
+import random
+import smtplib
+import datetime as dt
+
+
+MY_EMAIL = "truclhuynh87@gmail.com"
+MY_PASSWORD = 'Gookhongbiet88'
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 1:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readline()
+        quote = random.choice(all_quotes)
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        # encrypt the connection, secure it
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs='rongcon_armani@yahoo.com',
+            msg=f'Subject:Monday Motivation\n\n{quote}'
+        )
