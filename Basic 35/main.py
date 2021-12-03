b8f833835a1848c9d5f715bc1f4ea3b598b7f88a
TrucDev <jackyhuynh87@gmail.com>
2021-12-03 09:24:45 -0500

    Update main.py

--- Basic 35/main.py
+++ Basic 35/main.py
@@ -1,5 +1,12 @@
 import requests
 
 QWM_Endpoint = "api.openweathermap.org/data/2.5/weather"
-API_KEY=""
+API_KEY = "6f28ca2af5a05f520a94a7d20b061846"
 
+weather_params={
+    "lat":
+    "lon":
+    "appid":API_KEY
+}
+
+requests.get(QWM_Endpoint, params=)
