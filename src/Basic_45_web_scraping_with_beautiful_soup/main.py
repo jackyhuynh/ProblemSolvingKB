ebabc0d4b842eb5817ab45c6cbc4776eeb5ecf01
TrucDev <jackyhuynh87@gmail.com>
2021-12-10 16:59:16 -0500

    Update main.py

--- src/Basic_45_web_scraping_with_beautiful_soup/main.py
+++ src/Basic_45_web_scraping_with_beautiful_soup/main.py
@@ -17,3 +17,14 @@ soup = BeautifulSoup(contents, "html.parser")
 Find the info that we look for using Beautiful Soup
 """
 
+# all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
+# for tag in all_anchor_tags:
+#     print(tag.get("href"))
+#     # print(tag.getText())
+
+heading = soup.find(name="h1", id="name")
+# print(heading)
+
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading)
