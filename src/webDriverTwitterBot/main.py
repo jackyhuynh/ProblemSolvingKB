9aad70f2270a5e693476f04bb1042e99eade30b4
TrucDev <jackyhuynh87@gmail.com>
2021-12-11 13:32:47 -0500

    Update main.py

--- src/webDriverTwitterBot/main.py
+++ src/webDriverTwitterBot/main.py
@@ -1,16 +1,65 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+PROMISED_DOWN = 10
+PROMISED_UP = 50
+CHROME_DRIVER_PATH = r"C:\Users\jacky\Documents\Development\chromedriver_win32\chromedriver.exe"
+TWITTER_EMAIL = "USERNAME"
+TWITTER_PASSWORD = 'PASSWORD'
+# https://stackoverflow.com/questions/69875125/find-element-by-commands-are-deprecated-in-selenium/69875984
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+class InternetSpeedTwitterBot:
+    def __init__(self, driver_path):
+        self.driver = webdriver.Chrome(executable_path=driver_path)
+        self.up = 0
+        self.down = 0
+
+    def get_internet_speed(self):
+        self.driver.get("https://fast.com/")
+
+        time.sleep(10)
+        show_info = self.driver.find_element(By.ID, "show-more-details-link")
+        show_info.click()
+        self.up = self.driver.find_element(By.ID, 'speed-value')
+        self.down = self.driver.find_element(By.ID, 'upload-value')
+        print(self.down.text, self.up.text)
+
+    def tweet_at_provider(self):
+        self.driver.get("https://twitter.com/login")
+
+        time.sleep(2)
+        email = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[1]/label/div/div[2]/div/input')
+        email.send_keys(TWITTER_EMAIL)
+
+        time.sleep(2)
+        password = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[2]/label/div/div[2]/div/input')
+        password.send_keys(TWITTER_PASSWORD)
+
+        time.sleep(2)
+        password.send_keys(Keys.ENTER)
+
+        time.sleep(5)
+        tweet_compose = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div/div')
+
+        tweet = f"Hey Internet Provider, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?"
+        tweet_compose.send_keys(tweet)
+        time.sleep(3)
+
+        tweet_button = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]')
+        tweet_button.click()
+
+        time.sleep(2)
+        self.driver.quit()
+
+
+bot = InternetSpeedTwitterBot(CHROME_DRIVER_PATH)
+bot.get_internet_speed()
+bot.tweet_at_provider()
