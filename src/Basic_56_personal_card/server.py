c2301ac9f524398cfc372653c5c37c01539cd9eb
TrucDev <jackyhuynh87@gmail.com>
2021-12-12 15:41:29 -0500

    add static folder to render html code

--- src/Basic_56_personal_card/server.py
+++ src/Basic_56_personal_card/server.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello World!"
+    return render_template('truc.html')
 
 
 if __name__ == "__main__":
