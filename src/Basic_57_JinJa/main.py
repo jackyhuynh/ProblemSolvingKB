c2170431e9b3adfe461450b59f59ab8cff8884d2
TrucDev <jackyhuynh87@gmail.com>
2021-12-14 12:11:04 -0500

    update main

--- src/Basic_57_JinJa/main.py
+++ src/Basic_57_JinJa/main.py
@@ -2,10 +2,17 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
+block_url = "https://api.npoint.io/7011753364ea871e8b36"
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+
+@app.route('/post/<int:index>')
+def create_blog(index):
+    return render_template("post.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
