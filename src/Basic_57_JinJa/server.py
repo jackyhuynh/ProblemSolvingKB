0c3b7234a2e3a4efa7c83a259f5223c5320d265b
TrucDev <jackyhuynh87@gmail.com>
2021-12-13 20:38:32 -0500

    Update server.py

--- src/Basic_57_JinJa/server.py
+++ src/Basic_57_JinJa/server.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+import random
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    random_num = random.randint(1, 10)
+    current_year = datetime.now().year
+    return render_template('index.html', num=random_num, year=current_year)
 
 
 if __name__ == "__main__":
