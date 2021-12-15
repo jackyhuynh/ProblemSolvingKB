11e490be9addc5f4eed59ae92eb4b9f620706278
TrucDev <jackyhuynh87@gmail.com>
2021-12-15 05:22:19 -0500

    Update main.py

--- src/Basic_57_Blog/main.py
+++ src/Basic_57_Blog/main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask, render_template
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.route('/')
+def home():
+    return render_template("index.html")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.route('/post/<int:index>')
+def show_blog():
+    return render_template("post.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
