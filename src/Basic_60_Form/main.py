d8229cf71b360ba5c0a826651d8c964c8244a4a7
TrucDev <jackyhuynh87@gmail.com>
2021-12-16 16:00:33 -0500

    Update main.py
    description: start-server successfully

--- src/Basic_60_Form/main.py
+++ src/Basic_60_Form/main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+from flask import Flask, render_template
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
