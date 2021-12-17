6f9113c282215cd93f45e2566e53fd28faa43829
TrucDev <jackyhuynh87@gmail.com>
2021-12-16 20:55:39 -0500

    Update main.py

--- src/Basic_60_Form/main.py
+++ src/Basic_60_Form/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/login', methods=["POST"])
+def receive_data():
+    name = request.form["username"]
+    password = request.form["password"]
+    return f"<h1>Name: {name}, Password: {password}</h1>"
 
 
 if __name__ == '__main__':
