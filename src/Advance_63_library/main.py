fab4c22d217653f01b5664fa759bf0a6d8f7d94b
TrucDev <jackyhuynh87@gmail.com>
2021-12-20 13:23:57 -0500

    Update main.py

--- src/Advance_63_library/main.py
+++ src/Advance_63_library/main.py
@@ -22,21 +22,44 @@ db.create_all()
 @app.route('/')
 def home():
     all_books = db.session.query(Book).all()
-    return render_template('index.html', all_books=all_books)
+    return render_template('index.html', books=all_books)
 
 
 @app.route("/add", methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        new_book = {
-            'title': request.form['title'],
-            'author': request.form['author'],
-            'rating': request.form['rating']
-        }
-        all_books.append(new_book)
-
+        new_book = Book(title=request.form['title'], author=request.form['author'], rating=request.form['rating'])
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for('home'))
+
     return render_template('add.html')
+
+
+@app.route("/edit", methods=['GET', 'POST'])
+def edit():
+    if request.method == 'POST':
+        # Update records
+        book_id = request.form['id']
+        book_to_update = Book.query.get(book_id)
+        book_to_update.rating = request.form['rating']
+        db.session.commit()
+        return redirect(url_for('home'))
+
+    book_id = request.args.get('id')
+    book_selected = Book.query.get(book_id)
+    return render_template('edit_rating.html', book=book_selected)
+
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get('id')
+
+    # Delete a record by ID
+    book_to_delete = Book.query.get(book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
