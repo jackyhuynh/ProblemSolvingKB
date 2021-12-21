73817e5bd7ab1861b524c6888d47e246f41a00b9
TrucDev <jackyhuynh87@gmail.com>
2021-12-21 15:52:04 -0500

    Update main.py

--- src/Advance_63_Top_10_Movies/main.py
+++ src/Advance_63_Top_10_Movies/main.py
@@ -6,6 +6,12 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
+
+MOVIE_DB_API_KEY = '954f81fa074491f530740fbe1342a460'
+MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
@@ -46,7 +52,10 @@ class FindMovieForm(FlaskForm):
 
 @app.route("/")
 def home():
-    all_movies = Movie.query.all()
+    all_movies = Movie.query.order_by(Movie.rating).all()
+    for i in range(len(all_movies)):
+        all_movies[i].ranking = len(all_movies) - i
+    db.session.commit()
     return render_template("index.html", movies=all_movies)
 
 
@@ -75,7 +84,26 @@ def delete():
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     form = FindMovieForm()
+    if form.validate_on_submit():
+        movie_title = form.title.data
+        response = requests.get(MOVIE_DB_SEARCH_URL, params={"api_key": MOVIE_DB_API_KEY, "query": movie_title})
+        data = response.json()["results"]
+        return render_template('select.html', options=data)
     return render_template('add.html', form=form)
+
+
+@app.route('/find')
+def find_movie():
+    movie_api_id = request.args.get('id')
+    if movie_api_id:
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+        response = requests.get(movie_api_url, params={"api_key": MOVIE_DB_API_KEY, "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(title=data["title"], year=data["release_date"].split('-')[0],
+                          img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}", description=data["overview"])
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for("edit", id=new_movie.id))
 
 
 if __name__ == '__main__':
