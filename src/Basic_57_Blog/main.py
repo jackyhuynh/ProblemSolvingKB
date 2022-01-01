8515bfc0ce2b745746650dd7ed8b68d16f7da158
TrucDev <jackyhuynh87@gmail.com>
2022-01-01 11:23:06 -0500

    Update main.py

--- src/Basic_57_Blog/main.py
+++ src/Basic_57_Blog/main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from post import Post
 import requests
 
-posts = requests.get("https://api.npoint.io/5abcca6f4e39b4955965").json()
+posts = requests.get("https://api.npoint.io/57b20cb9ccbbf28da6db").json()
 post_objects = []
 for post in posts:
     post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
