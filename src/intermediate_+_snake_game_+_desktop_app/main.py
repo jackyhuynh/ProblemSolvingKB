31720432398394dc6580003fa7fce08768bf9e2c
TrucDev <jackyhuynh87@gmail.com>
2022-01-06 10:31:37 -0500

    Create main.py

---
+++ src/intermediate_+_snake_game_+_desktop_app/main.py
@@ -0,0 +1,57 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("My Snake Game")
+screen.bgcolor("black")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+"""
+The screen listen create a new listen object that interrupt the while loop inside the game
+"""
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    # Screen refreshing
+    time.sleep(0.1)
+    # Start the snake move straight
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+        snake.extend()
+
+    x_coordinate = int(snake.head.xcor())
+    y_coordinate = int(snake.head.ycor())
+    print(x_coordinate, y_coordinate)
+
+    # Detect collision with wall
+    if x_coordinate > 280 or x_coordinate < -280 \
+            or y_coordinate > 280 or y_coordinate < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with self
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+screen.exitonclick()
